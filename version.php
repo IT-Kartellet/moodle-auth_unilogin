4484ef8fe169c6734320e75edd39f332db266ef2
Jan Aagaard Meier <janzeh@gmail.com>
2015-02-24 17:23:54 +0100

    Release new version

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015022400;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015022401;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013040500;        // Requires this Moodle version
 $plugin->component = 'auth_unilogin';    // Full name of the plugin (used for diagnostics)
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release  = 'v1.0.0';
+$plugin->release  = 'v1.0.1';
