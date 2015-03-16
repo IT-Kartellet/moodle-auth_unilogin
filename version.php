aa5c03c47d3737f072e6849e28f8d087231cfed2
Jan Aagaard Meier <janzeh@gmail.com>
2015-03-16 08:44:45 +0100

    Update version in version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015022401;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015031601;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013040500;        // Requires this Moodle version
 $plugin->component = 'auth_unilogin';    // Full name of the plugin (used for diagnostics)
 
